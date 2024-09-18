03aec35372ae2bc39565b616ec79ff90ec020777
Andrew <andrewbryson12@gmail.com>
2024-09-18 17:00:17 +0200

    test/screens update

--- test/screens/index.ts
+++ test/screens/index.ts
@@ -13,15 +13,14 @@ const init = async () => {
   for await (const size of TEST_SCREENS_SIZES) {
     await page.setViewport(size);
 
-    const website_url = 'http://localhost:3000/dashboard';
+    const website_url =
+      'https://www.takeshapehome.com/dashboard';
     await page.goto(website_url, {
       waitUntil: ['load', 'networkidle0'],
     });
-    setTimeout(async () => {
-      await page.screenshot({
-        path: `${TEST_SCREENS_DIR}/_${size.width}x${size.height}.jpg`,
-      });
-    }, 1000);
+    await page.screenshot({
+      path: `${TEST_SCREENS_DIR}/_${size.width}x${size.height}.jpg`,
+    });
   }
 
   await browser.close();
