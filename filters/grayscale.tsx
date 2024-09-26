5325ad044a2798bca4f3dc35002e0096aa8ba725
Andrew <andrewbryson12@gmail.com>
2024-09-26 15:17:42 +0200

    changed color slider

--- filters/grayscale.tsx
+++ filters/grayscale.tsx
@@ -1,43 +1,24 @@
-import { LANDING_HERO_HANDLE_SIZE } from '@/components/landing/hero/constants';
-import {
-  motion,
-  MotionValue,
-  useTransform,
-} from 'framer-motion';
+import { motion, MotionValue } from 'framer-motion';
 import type { FC } from 'react';
 export const FILTER_GRAYSCALE_ID = 'FILTER_GRAYSCALE_ID';
 
 type TProps = { x: MotionValue };
 export const FilterGrayscale: FC<TProps> = ({ x }) => {
-  // const value = useTransform(x, (v) => {
-  //   console.log(v);
-  //   return v;
-  // });
   return (
     <svg
       width="0"
       height="0"
-      viewBox="0 0 1 1"
+      viewBox="0 0 100 100"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <motion.filter id={FILTER_GRAYSCALE_ID}>
-        <feColorMatrix
-          type="saturate"
-          values="0.05"
-          result="A"
-        />
-        <motion.feComposite
-          operator="in"
-          result="A"
-          x="0"
-          width={x}
-        />
-        <feComposite
-          operator="over"
-          in2="SourceGraphic"
-          in="A"
-        />
-      </motion.filter>
+      <filter id={FILTER_GRAYSCALE_ID}>
+        <feColorMatrix type="saturate" values="0.05" />
+        <motion.feComposite operator="in" x="0" width={x} />
+        <feMerge>
+          <feMergeNode in="SourceGraphic" />
+          <feMergeNode />
+        </feMerge>
+      </filter>
     </svg>
   );
 };
